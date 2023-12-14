56075c2cb708eeb479cb3bdd47be727d72addc85
Max Gladlov <m.gladkov@avtopass.net>
2023-12-15 02:40:05 +0300

    jaeger, prometheus, grafana

--- document_service/app/main.py
+++ document_service/app/main.py
@@ -9,7 +9,38 @@ import app.db.database as database
 app = FastAPI()
 database.Base.metadata.create_all(bind=database.engine)
 
+#######
+#Jaeger
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import  TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
+resource = Resource(attributes={
+    SERVICE_NAME: "document-service"
+})
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831
+)
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(jaeger_exporter)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+FastAPIInstrumentor.instrument_app(app)
+
+###########
+#Prometheus
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
+
+###########
+#main
 def get_db():
     db = database.SessionLocal()
     try:
--- user_service/app/main.py
+++ user_service/app/main.py
@@ -7,6 +7,40 @@ from uuid import UUID
 # uvicorn user_service.main:user_service --reload
 app = FastAPI()
 
+#######
+#Jaeger
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import  TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+resource = Resource(attributes={
+    SERVICE_NAME: "user-service"
+})
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831
+)
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(jaeger_exporter)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+FastAPIInstrumentor.instrument_app(app)
+
+###########
+#Prometheus
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
+
+
+##########
+#main
+
 db: List[User] = [
     User(
         id=UUID('86f053a0-0dd1-4439-ba43-bdf586220bd2'),
